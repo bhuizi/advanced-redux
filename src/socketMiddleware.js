fed5fcc5d8b5122c6d2f33e2c6c7a8d6b268bd24
bryan huizi <bryanhuizi@gmail.com>
2018-02-09 10:10:06 -0800

    05: fixes typo

--- src/socketMiddleware.js
+++ src/socketMiddleware.js
@@ -1,6 +1,6 @@
 export const createSocketMiddleware = io => config => {
   const socket = io();
-  return sotre => next => action => {
+  return store => next => action => {
     for (const key in config) {
       if (action.type === key) {
         socket.emit(config[key](action));
